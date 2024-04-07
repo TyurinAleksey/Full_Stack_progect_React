411fb078ef596c2ea759bdadb8ef0bd25285f3aa
mechtatel <alaxche@gmail.com>
2024-04-07 17:22:52 +1000

    createPosts

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import {registerValidation, loginValidation, postCreateValidation} from "./validations.js";
 
@@ -27,6 +28,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 app.use(express.json());
+app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 app.post("/auth/login", loginValidation, handelValidationsErrors, UserController.login);
@@ -39,7 +41,10 @@ app.post("/uploads", checkAuth, upload.single("image"), (req, res) => {
     })
 })
 
+app.get("/tags", PostController.getLastTags);
+
 app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
 app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, handelValidationsErrors, PostController.create);
 app.delete("/posts/:id", checkAuth, PostController.remove);
